69e7601a7649c1dea5fc96dece6d23473c5e1fde
srahul3 <rahulsharma@hashicorp.com>
2024-07-15 10:57:09 +0530

    cleanup

--- main.tf
+++ main.tf
@@ -7,16 +7,18 @@ provider "aws" {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "2.78.0"
 
-  name                 = "neo4j_vpc"
-  cidr                 = "10.0.0.0/16"
-  azs                  = ["us-west-2a"]
-  private_subnets      = ["10.0.1.0/24"]
-  public_subnets       = ["10.0.2.0/24"]
+  name = "neo4j_vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a"]
+  private_subnets = ["10.0.1.0/24"]
+  public_subnets  = ["10.0.2.0/24"]
+
   enable_nat_gateway   = true
-  single_nat_gateway   = true
+  enable_vpn_gateway   = true
   enable_dns_hostnames = true
+  enable_dns_support   = true
 }
 
 # Create a security group
